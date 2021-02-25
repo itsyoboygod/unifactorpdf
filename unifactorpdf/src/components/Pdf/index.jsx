820407606dcbb4ea09d78739b63953ca0a76b338
itsyoboygod <m.vini.ac@hotmail.com>
2021-02-25 16:24:57 -0300

    all

--- unifactorpdf/src/components/Pdf/index.jsx
+++ unifactorpdf/src/components/Pdf/index.jsx
@@ -1,10 +1,8 @@
 import { render } from "@testing-library/react";
 import React from "react";
 
-const PDF = () =>{
-    render(){
-        return(
-            <h1>BRUH </h1>
-        )
-    }
-}
+const PDF = () => {
+  return <h1>BRUH </h1>;
+};
+
+export default PDF;
