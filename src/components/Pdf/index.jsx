40ade468d34458714ed04a5b908c28be71de91f5
itsyoboygod <m.vini.ac@hotmail.com>
2021-02-25 23:38:35 -0300

    ta pelo

--- src/components/Pdf/index.jsx
+++ src/components/Pdf/index.jsx
@@ -1,12 +1,13 @@
 import React from "react";
-import Pdf from "react-to-pdf"
+import Pdf from "react-to-pdf";
+import "./style.css";
 
 const ref = React.createRef();
 
 const PDF = (props) => {
   return (
     <>
-      <div className="Post" ref={ref}>
+      <div className="post" ref={ref}>
         <h1>{props.project}</h1>
         <h1>{props.firstname}</h1>
         <h1>{props.lastname}</h1>
@@ -19,7 +20,7 @@ const PDF = (props) => {
 
   </div>
       <Pdf targetRef={ref} filename="post.pdf">
-        {({ toPdf }) => <button onClick={toPdf}>Create PDF</button>}
+        {({ toPdf }) => <button id="create-pdf" onClick={toPdf}>Create PDF</button>}
       </Pdf>
     </>
   )
