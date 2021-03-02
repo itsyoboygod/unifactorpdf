16c817d548f815810b6dcb49d6bf50828c888b13
itsyoboygod <m.vini.ac@hotmail.com>
2021-03-02 00:01:47 -0300

    modified:   src/components/Pdf/index.jsx

--- src/components/Pdf/index.jsx
+++ src/components/Pdf/index.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import Pdf from "react-to-pdf";
 import "./style.css";
-import { Page, Text, View, Document, StyleSheet } from "@react-pdf/renderer";
+import { Page, Text, View, Document, StyleSheet, PDFViewer } from "@react-pdf/renderer";
 
-import { PDFViewer } from "@react-pdf/renderer";
+// import { PDFViewer } from "@react-pdf/renderer";
 
 const ref = React.createRef();
 
@@ -11,91 +11,94 @@ const styles = StyleSheet.create({
   page: {
     flexDirection: "row",
     backgroundColor: "#E4E4E4",
-  },
-  section: {
-    margin: 10,
-    padding: 10,
-    flexGrow: 1,
-  },
+    minHeight: '100%'
+  }
 });
 
-const PDF = (props) => {
-  return (
-    <>
-      <PDFViewer>
-        <View>
-          <div className="document" ref={ref}>
-            <div className="header">
-              <h1>header</h1>
-            </div>
-
-            <div className="post">
-              <div className="left">
-                <p>INFORMAÇÃO GERAL</p>
-                <h2>Nome do projeto:</h2>
-                <h1>{props.project}</h1>
-                <h2>Nome do proprietário:</h2>
-                <h1>{props.firstname}</h1>
-                <h2>Sobrenome do proprietário:</h2>
-                <h1>{props.lastname}</h1>
-                <h2>Email do proprietário:</h2>
-                <h1>{props.email}</h1>
-                <h2>Contato do proprietário:</h2>
-                <h1>{props.contact}</h1>
-                <h2>Endereço do proprietário:</h2>
-                <h1>{props.end}</h1>
-                <h2>Local de retirado do produto:</h2>
-                <h1>{props.retirada}</h1>
-
-                <p>INFORMAÇÃO GERAL</p>
-                <h2>Nome do projeto:</h2>
-                <h1>{props.project}</h1>
-                <h2>Nome do proprietário:</h2>
-                <h1>{props.firstname}</h1>
-                <h2>Sobrenome do proprietário:</h2>
-                <h1>{props.lastname}</h1>
-                <h2>Email do proprietário:</h2>
-                <h1>{props.email}</h1>
-                <h2>Contato do proprietário:</h2>
-                <h1>{props.contact}</h1>
-                <h2>Endereço do proprietário:</h2>
-                <h1>{props.end}</h1>
-                <h2>Local de retirado do produto:</h2>
-                <h1>{props.retirada}</h1>
-              </div>
-              <div className="right">
-                <p>INFORMAÇÃO TÉCNICA</p>
-                <h2>Produto:</h2>
-                <h1>{props.product}</h1>
-                <h2>Unidades:</h2>
-                <h1>{props.units}</h1>
-                <h2>Peso do produto:</h2>
-                <h1>{props.weight}</h1>
-                <h2>Cor do produto:</h2>
-                <h1>{props.color}</h1>
-              </div>
-            </div>
-
-            <div className="footer">
-              <div className="uni-logo">
-                <p>UNIFACTOR</p>
-                <span>3D</span>
-              </div>
-              <h3>SÃO PAULO</h3>
-            </div>
-          </div>
+const Doc = (props) => (
+  <Document>
+    <Page size="A4">
+      <View className="document" ref={ref} style={{ margin: 0, padding: 0, top: 0 }}>
+        <View className="header" style={{ margin: 0, padding: 0, top: 0 }}>
+          <Text>header</Text>
         </View>
-      </PDFViewer>
 
-      <Pdf size="A4" targetRef={ref} filename="post.pdf">
-        {({ toPdf }) => (
-          <button id="create-pdf" onClick={toPdf} target="_blank">
-            Create PDF
-          </button>
-        )}
-      </Pdf>
-    </>
-  );
-};
+        <View className="post">
+          <View className="left">
+            <Text>INFORMAÇÃO GERAL</Text>
+            <Text>Nome do projeto:</Text>
+            <Text>{props.project}</Text>
+            <Text>Nome do proprietário:</Text>
+            <Text>{props.firstname}</Text>
+            <Text>Sobrenome do proprietário:</Text>
+            <Text>{props.lastname}</Text>
+            <Text>Email do proprietário:</Text>
+            <Text>{props.email}</Text>
+            <Text>Contato do proprietário:</Text>
+            <Text>{props.contact}</Text>
+            <Text>Endereço do proprietário:</Text>
+            <Text>{props.end}</Text>
+            <Text>Local de retirado do produto:</Text>
+            <Text>{props.retirada}</Text>
+
+            <Text>INFORMAÇÃO GERAL</Text>
+            <Text>Nome do projeto:</Text>
+            <Text>{props.project}</Text>
+            <Text>Nome do proprietário:</Text>
+            <Text>{props.firstname}</Text>
+            <Text>Sobrenome do proprietário:</Text>
+            <Text>{props.lastname}</Text>
+            <Text>Email do proprietário:</Text>
+            <Text>{props.email}</Text>
+            <Text>Contato do proprietário:</Text>
+            <Text>{props.contact}</Text>
+            <Text>Endereço do proprietário:</Text>
+            <Text>{props.end}</Text>
+            <Text>Local de retirado do produto:</Text>
+            <Text>{props.retirada}</Text>
+          </View>
+          <View className="right">
+            <Text>INFORMAÇÃO TÉCNICA</Text>
+            <Text>Produto:</Text>
+            <Text>{props.product}</Text>
+            <Text>Unidades:</Text>
+            <Text>{props.units}</Text>
+            <Text>Peso do produto:</Text>
+            <Text>{props.weight}</Text>
+            <Text>Cor do produto:</Text>
+            <Text>{props.color}</Text>
+          </View>
+        </View>
+
+        <View className="footer">
+          <View className="uni-logo">
+            <Text>UNIFACTOR</Text>
+            <Text>3D</Text>
+          </View>
+          <Text>SÃO PAULO</Text>
+        </View>
+      </View>
+    </Page>
+  </Document>
+);
+
+// {({ toPdf }) => (
+//   <button id="create-pdf" onClick={toPdf} target="_blank">
+//     Create PDF
+//   </button>
+// )}
+
+function PDF() {
+  return (
+    <div style={{ height: '99vh' }}>
+      <PDFViewer size="A4" targetRef={ref} filename="post.pdf" height="100%" width="99%">
+        <Doc />
+      </PDFViewer>
+    </div>
+  )
+}
+
+
+
 
 export default PDF;
