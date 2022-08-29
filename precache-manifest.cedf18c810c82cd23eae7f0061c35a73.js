f8007f6246771fe7152e835ebb36ab00a6d25e99
itsyoboygod <m.vini.ac@hotmail.com>
2022-08-29 13:54:20 -0300

    Updates

---
+++ precache-manifest.cedf18c810c82cd23eae7f0061c35a73.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "aa72cc35e3afb477e47c",
+    "url": "/unifactorpdf/static/css/main.87bc1ab5.chunk.css"
+  },
+  {
+    "revision": "aa72cc35e3afb477e47c",
+    "url": "/unifactorpdf/static/js/main.aa72cc35.chunk.js"
+  },
+  {
+    "revision": "125e4e37ed6b5973247a",
+    "url": "/unifactorpdf/static/js/1.125e4e37.chunk.js"
+  },
+  {
+    "revision": "f7f1c9ed483b5f8537b6",
+    "url": "/unifactorpdf/static/js/runtime~main.f7f1c9ed.js"
+  },
+  {
+    "revision": "a720f17aa773e493a7ebf8b08459e66c",
+    "url": "/unifactorpdf/static/media/Roboto-Italic.a720f17a.ttf"
+  },
+  {
+    "revision": "411051cb61bcda5517943601e8734cea",
+    "url": "/unifactorpdf/static/media/Monoton-Regular.411051cb.ttf"
+  },
+  {
+    "revision": "e07df86cef2e721115583d61d1fb68a6",
+    "url": "/unifactorpdf/static/media/Roboto-Bold.e07df86c.ttf"
+  },
+  {
+    "revision": "c6e66df05b340ff75c2d8abb22471279",
+    "url": "/unifactorpdf/index.html"
+  }
+];
